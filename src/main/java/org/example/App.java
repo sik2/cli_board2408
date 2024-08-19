91f866b419b734b06300779b8b0dd5d79d480f5d
sik2 <bickwang123@gmail.com>
2024-08-19 12:44:19 +0900

    2nd complete

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,10 +12,15 @@ public class App {
         System.out.println("== 게시판 앱 ==");
         while (true) {
             System.out.print("명령) ");
-            String command = sc.nextLine();
+            String command = sc.nextLine().trim();
 
             if (command.equals("종료")) {
                 break;
+            } else if (command.equals("등록")) {
+                System.out.print("제목 : ");
+                String subject = sc.nextLine().trim();
+                System.out.print("내용 : ");
+                String content = sc.nextLine().trim();
             }
         }
     }
