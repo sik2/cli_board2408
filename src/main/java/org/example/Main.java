cad4085011d6f69725a3625c6f6ea963e8f28bf9
sik2 <bickwang123@gmail.com>
2024-08-19 12:33:07 +0900

    1-2 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 게시판 앱 ==");
         Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("명령) ");
-            String command = sc.nextLine();
-
-            if (command.equals("종료")) {
-                break;
-            }
-        }
+        new App(sc).run();
+        sc.close();
     }
 }
