c2cc0497939b2b315df03b1ad2579e78dad77176
sik2 <bickwang123@gmail.com>
2024-08-19 12:24:37 +0900

    1-1 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HI1231232");
+        System.out.println("== 게시판 앱 ==");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine();
+
+            if (command.equals("종료")) {
+                break;
+            }
+        }
     }
 }
